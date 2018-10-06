0c2346aa6cd26b2fdd6d6feb9cacaf4f75f6f81d
Abhishek <abhisheksinghal7599@gmail.com>
2018-10-06 10:46:00 +0530

    Add delete all and content provider in manifest

--- app/src/main/java/com/example/abhishek/inventorymanager/MainActivity.java
+++ app/src/main/java/com/example/abhishek/inventorymanager/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.abhishek.inventorymanager;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.preference.EditTextPreference;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -12,16 +17,21 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.abhishek.inventorymanager.data.ItemContract;
 import com.example.abhishek.inventorymanager.data.ItemDbHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
-    ItemDbHelper mDbHelper;
+    private static final int ITEM_LOADER = 0;
+    ItemCursorAdapter mCursorAdapter;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -33,63 +43,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
 
-    private void displayDatabaseInfo(){
-        mDbHelper = new ItemDbHelper(this);
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        String[] projection = {ItemContract.ItemEntry._ID,
-                ItemContract.ItemEntry.COLUMN_ITEM_NAME,
-                ItemContract.ItemEntry.COLUMN_ITEM_PRICE,
-                ItemContract.ItemEntry.COLUMN_ITEM_QUANTITY,
-                ItemContract.ItemEntry.COLUMN_ITEM_SUPPLIER,
-                ItemContract.ItemEntry.COLUMN_ITEM_SUPPLIER_PHONE};
-        String selection = null;
-        String[] seletionArgs = null;
-        Cursor cursor = db.query(ItemContract.ItemEntry.TABLE_NAME,
-                projection,
-                selection,
-                seletionArgs,
-                null,null,null);
-        TextView displayView = (TextView) findViewById(R.id.text_view_item);
-        try{
-            displayView.setText("Number of rows = " + cursor.getCount()+ "\n");
-
-            int idColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_ITEM_NAME);
-            int quantityColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_ITEM_QUANTITY);
-            int priceColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_ITEM_PRICE);
-            int supplierColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_ITEM_SUPPLIER);
-            int phoneColumnIndex = cursor.getColumnIndex(ItemContract.ItemEntry.COLUMN_ITEM_SUPPLIER_PHONE);
-            Log.e("MainActivityAAAAAAAA",Integer.toString(nameColumnIndex));
-
-            while (cursor.moveToNext()){
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                int currentPrice = cursor.getInt(priceColumnIndex);
-                int currentQuantity = cursor.getInt(quantityColumnIndex);
-                String currentSupplier = cursor.getString(supplierColumnIndex);
-                String currentSupplierPhone = cursor.getString(phoneColumnIndex);
-                displayView.append("\n" + currentID + " - "+
-                                        currentName + "\t" +
-                                        currentPrice+ "\t" +
-                                        currentQuantity+ "\t" +
-                                        currentSupplier + "\t" +
-                                        currentSupplierPhone);
+        ListView itemListView = (ListView) findViewById(R.id.list_view_items);
+        View emptyView = findViewById(R.id.empty_view);
+        itemListView.setEmptyView(emptyView);
+        mCursorAdapter = new ItemCursorAdapter(this, null);
+        itemListView.setAdapter(mCursorAdapter);
+        itemListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                Uri currentUri = ContentUris.withAppendedId(ItemContract.ItemEntry.CONTENT_URI, id);
+                intent.setData(currentUri);
+                startActivity(intent);
             }
-        }
-        catch (Exception e){
-            Log.e("MainActivityERROR",e.getMessage());
-        }
-        finally {
-            cursor.close();
-        }
+        });
+
+        getLoaderManager().initLoader(ITEM_LOADER, null, this);
     }
 
     @Override
@@ -99,18 +69,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void insert_item(){
-        mDbHelper = new ItemDbHelper(this);
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
         ContentValues contentValues = new ContentValues();
         contentValues.put(ItemContract.ItemEntry.COLUMN_ITEM_NAME,"A brief History of time");
         contentValues.put(ItemContract.ItemEntry.COLUMN_ITEM_PRICE,400);
         contentValues.put(ItemContract.ItemEntry.COLUMN_ITEM_QUANTITY,10);
         contentValues.put(ItemContract.ItemEntry.COLUMN_ITEM_SUPPLIER,"Penguin");
         contentValues.put(ItemContract.ItemEntry.COLUMN_ITEM_SUPPLIER_PHONE,"8987456210");
-        long new_row_id = db.insert(ItemContract.ItemEntry.TABLE_NAME, null, contentValues);
-        Log.v("MainActivityIDPRINT", new_row_id+"");
-        displayDatabaseInfo();
+        Uri newUri = getContentResolver().insert(ItemContract.ItemEntry.CONTENT_URI, contentValues);
+    }
+
+    private void deleteAllItems(){
+        int rowsDeleted = getContentResolver().delete(ItemContract.ItemEntry.CONTENT_URI, null, null);
+        if(rowsDeleted>0)
+            Toast.makeText(this, "All Items deleted", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Deletion failed", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -121,8 +94,29 @@ public class MainActivity extends AppCompatActivity {
                 //finish();
                 return true;
             case R.id.action_delete_all_entries:
+                deleteAllItems();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = new String[]{
+                ItemContract.ItemEntry._ID,
+                ItemContract.ItemEntry.COLUMN_ITEM_NAME,
+                ItemContract.ItemEntry.COLUMN_ITEM_QUANTITY};
+
+        return new CursorLoader(this, ItemContract.ItemEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
